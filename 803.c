271509b2da394887b5b1a0cfc4b0cdcbd246ea30
Laura <120367965+LCAW@users.noreply.github.com>
2023-01-16 14:31:50 +0100

    Update 803.c

--- 803.c
+++ 803.c
@@ -1,5 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
-
-int main (){
-  
+int main()
+{
+    int i,a;
+   char* vn[5];
+   vn[0]="Laura";
+   vn[1]="Sharon";
+   vn[2]="Marlon";
+   vn[3]="Marius";
+   vn[4]="Janine";
+   
+   char* nn[5];
+   nn[0]="Weitmann";
+   nn[1]="Buch";
+   nn[2]="Schmidt";
+   nn[3]="Maurer";
+   nn[4]="Reidenbach";
+   
+   for(a==0;a<5;a++){
+   printf("%s",vn[a]);
+   if(a<5){
+    for(i==0;i<80;i++){
+        
+       printf("_");}}
+       else{}
+   
+   printf("%s\n",nn[a]);}
+   
+   
+   
